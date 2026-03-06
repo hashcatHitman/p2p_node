1cc8e5d48ae73dd250708b2bd796995f76ba44ae
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2026-03-06 00:19:33 -0500

    fix(heartbeat): Check before adding peer
    We don't want to overwrite existing entries with fresh records.
    
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

--- src/algorithms/heartbeat.rs
+++ src/algorithms/heartbeat.rs
@@ -112,7 +112,9 @@ impl HeartbeatNode {
 
     /// Register a new peer to monitor (start in ALIVE state).
     pub fn add_peer(&mut self, node_id: String) {
-        drop(self.peers.insert(node_id.clone(), PeerState::new(node_id)));
+        if !self.peers.contains_key(&node_id) {
+            drop(self.peers.insert(node_id.clone(), PeerState::new(node_id)));
+        }
     }
 
     /// Record that we are sending a PING to every ALIVE and SUSPECT peer.
