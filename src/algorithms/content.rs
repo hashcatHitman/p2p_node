b65e367a095da7306e5a03a25cfae73eadf5a0ab
hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>
2026-03-10 15:22:06 -0400

    feat(content): Add `ViewEvent`
    Signed-off-by: hashcatHitman <155700084+hashcatHitman@users.noreply.github.com>

---
+++ src/algorithms/content.rs
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: Copyright © 2025 hashcatHitman
+//
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! The content module is for data types relating to view events and audits.
+
+#[derive(Debug, Clone)]
+pub struct ViewEvent {
+    event_id: String,
+    content_id: String,
+    count: u64,
+    ad_id: Option<String>,
+}
+
+impl ViewEvent {
+    pub const fn new(
+        event_id: String,
+        content_id: String,
+        count: u64,
+        ad_id: Option<String>,
+    ) -> Self {
+        Self {
+            event_id,
+            content_id,
+            count,
+            ad_id,
+        }
+    }
+
+    pub fn event_id(&self) -> &str {
+        &self.event_id
+    }
+
+    pub fn content_id(&self) -> &str {
+        &self.content_id
+    }
+
+    pub const fn count(&self) -> u64 {
+        self.count
+    }
+
+    pub fn ad_id(&self) -> Option<&str> {
+        self.ad_id.as_deref()
+    }
+}
